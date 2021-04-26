13583e59a46fcd39ce6eadefe8690c0a4aed8710
Mark Pasricha <markp862@gmail.com>
2021-04-26 18:48:06 -0400

    adding files to files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,6 +4,9 @@ const FILES_TO_CACHE = [
 	"/", 
 	"/index.html",
 	"/styles.css",
+	"/index.js", 
+	"/indexdb.js", 
+	"manifest.json",
 	"/icons/icon-192x192.png", 
 	"/icons/icon-512x512.png", 
  	
