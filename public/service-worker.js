c76ac36006597fb84c0180884e64da7aeb9b31b4
Mark Pasricha <markp862@gmail.com>
2021-04-26 18:58:04 -0400

    d

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,12 +2,12 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1"; 
 const FILES_TO_CACHE = [
 	"/", 
-	"./index.html",
-	"./styles.css",
-	"./index.js", 
-	"./indexdb.js", 
-	"./manifest.json",
-	"../routes/api.js",
+	"/index.html",
+	"/styles.css",
+	"/index.js", 
+	"/indexdb.js", 
+	"/manifest.json",
+	"/routes/api.js",
 	"/icons/icon-192x192.png", 
 	"/icons/icon-512x512.png", 
  	
