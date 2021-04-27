02e7cd00b47e260ef20ca92f3be68601242de084
Mark Pasricha <markp862@gmail.com>
2021-04-26 20:20:07 -0400

    testing

--- routes/api.js
+++ routes/api.js
@@ -22,14 +22,14 @@ router.post("/api/transaction/bulk", ({body}, res) => {
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      console.log("In Error")
-      // res.status(404).json(err);
-    });
+  res.send("Hello")
+  // Transaction.find({}).sort({date: -1})
+  //   .then(dbTransaction => {
+  //     res.json(dbTransaction);
+  //   })
+  //   .catch(err => {
+  //     res.status(404).json(err);
+  //   });
 });
 
 module.exports = router;
